b62144c87efd69a4ee9a0675a4f2c6cd69a7472f
Richard King <richrdkng@gmail.com>
2015-05-07 21:06:58 +0200

    Clean generated assets (test updated asset tasks)

--- task/helpers/task.helper.js
+++ task/helpers/task.helper.js
@@ -1,34 +1,73 @@
-var taskRunner;
+var Gulp;
+
+function addTask(gulp, task) {
+    var args;
+
+    if(task instanceof Task) {
+        args = [
+            task.getName(),
+            task.getDependencies()
+        ];
+
+        if(task.hasFunc()) {
+            args.push(task.getFunc());
+        }
+
+        gulp.task.apply(gulp, args);
+
+    } else if(Object.prototype.toString.call(task) === '[object Object]') {
+        for(var k in task) {
+            if(task.hasOwnProperty(k)) {
+                addTask(gulp, task[k]);
+            }
+        }
+    }
+}
 
 function Task(name, dependencies, func) {
     this.name = name;
+
+    // no dependencies, task function as 2. argument
+    if(typeof dependencies === 'function') {
+        func = dependencies;
+        dependencies = [];
+    }
+
+    // dependencies only, no task function
+    if(typeof func === 'undefined') {
+        func = false;
+    }
+
     this.dependencies = dependencies;
     this.func = func
 }
 
-Task.prototype.getName = function() {
-    return this.name;
+Task.prototype = {
+    constructor: Task,
+
+    getName: function() {
+        return this.name;
+    },
+    getDependencies: function() {
+        return this.dependencies;
+    },
+    getFunc: function() {
+        return this.func;
+    },
+    hasFunc: function() {
+        return this.func !== false;
+    },
+    addTo: function(gulp) {
+        addTask(gulp, this);
+    }
 };
 
-Task.prototype.addTo = function(gulp) {
-    gulp.task(
-        this.name,
-        this.dependencies,
-        this.func
-    );
-};
-
-Task.to = function(tr) {
-    taskRunner = tr;
+Task.to = function(gulp) {
+    Gulp = gulp;
     return Task;
 };
-
-Task.addTask = function(task) {
-    taskRunner.task(
-        task.name,
-        task.dependencies,
-        task.func
-    );
+Task.add = function(task) {
+    addTask(Gulp, task);
     return Task;
 };
 
--- task/index.js
+++ task/index.js
@@ -7,25 +7,25 @@ var project = require('../project'),
     Task = require('./helpers/task.helper'),
     gulp = require('gulp'),
 
-    // Asset compilation tasks
-    asset_image  = require('./compile-image.task'),
-    asset_style  = require('./compile-style.task'),
-    asset_script = require('./compile-script.task'),
-    asset_markup = require('./compile-markup.task'),
+    // Asset management tasks
+    manage_image    = require('./manage-image.task.js'),
+    manage_style    = require('./manage-style.task.js'),
+    manage_script   = require('./manage-script.task.js'),
+    manage_markup   = require('./manage-markup.task.js'),
 
-    // Metadata compilation tasks
-    meta_package = require('./compile-metadata-package.task'),
+    // Metadata management tasks
+    meta_package    = require('./compile-metadata-package.task'),
 
     genpackage      = require('./compile-metadata-package.task.js'),
 
     log             = console.log;
 
 Task.to(gulp)
-    // Add asset compilation tasks
-    .addTask(asset_image)
-    .addTask(asset_style)
-    .addTask(asset_script)
-    .addTask(asset_markup)
+    // Add asset management tasks
+    .add(manage_image)
+    .add(manage_style)
+    .add(manage_script)
+    .add(manage_markup)
 
     // Add metadata compilation tasks
-    .addTask(meta_package);
+    .add(meta_package);
